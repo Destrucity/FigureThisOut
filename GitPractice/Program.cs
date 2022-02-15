b4e73637502845f24ff162d3051e1ba480261b5b
Destrucity <95849406+Destrucity@users.noreply.github.com>
2022-02-15 15:55:12 -0500

    Add project files.

---
+++ GitPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, World!");
+
+NumMonster cookie = new NumMonster();
+
+class Number
+{
+    public static int RandomNumber()
+    {
+        return new Random().Next(1, 101);
+    }
+}
+
+class NumMonster
+{
+    public int num;
+    public NumMonster()
+    {
+        num = Number.RandomNumber();
+        Console.WriteLine(num);
+    }
+}
